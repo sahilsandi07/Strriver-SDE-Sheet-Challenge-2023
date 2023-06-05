3378f4134aa7f6d117e938a86451e8635b95b69c
Sahil Sandi <officialsahilsandi@gmail.com>
2023-06-05 13:10:01 +0530

    Sort in O(1)

---
+++ sort012.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void sort012(int *arr, int n)
+{
+    int low = 0;
+    int mid = 0;
+    int high = n - 1;
+
+    while (mid <= high)
+    {
+        if (arr[mid] == 0)
+        {
+            std::swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            std::swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
